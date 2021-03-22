193f237c311c925cbc7aacd18dfb585e0b0d34d6
I'm Heinrich Lunge <79764954+imheinrichlunge@users.noreply.github.com>
2021-03-22 01:01:49 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const metaTags = document.getElementsByTagName("meta");
+let isMetaViewportThere = false;
+for (let i = 0; i < metaTags.length; i++) {
+    if (metaTags[i].getAttribute("name") === "viewport") {
+        isMetaViewportThere = true;
+        break;
+    }
+}
+if (!isMetaViewportThere) {
+    const headTag = document.head,
+        metaViewport = document.createElement("meta");
+    metaViewport.setAttribute("name", "viewport");
+    metaViewport.setAttribute("content", "width=device-width, initial-scale=1.0");
+    headTag.appendChild(metaViewport);
+}
